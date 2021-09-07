70cf7724bbff022849dd713fd84c32e4b195dc81
DavidGabelgaard <davi493c@edu.easj.dk>
2021-09-07 14:28:40 +0200

    Task done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,21 @@ package com.company;
 
 public class Main {
 
+
+
+
+
     public static void main(String[] args) {
-	// write your code here
+        String Day = "Thursday";
+        int Date = 2;
+        String Month = "September";
+        int Year = 2021;
+
+	    System.out.println("Hello World!");
+
+
+        System.out.println("American format: " + Day + ", " + Month + " " +  Date + ", " + Year);
+        System.out.println("European format: " + Day + Date + Month + Year);
+
     }
 }
